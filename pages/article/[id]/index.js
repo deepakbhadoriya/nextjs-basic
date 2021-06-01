f12e5bae63e6118e0e9f7fe1d11a0163679dceb6
Deepak Bhadoriya <deepakbhadoriya@Deepaks-MacBook-Pro.local>
2021-06-01 12:12:19 +0530

    removed get static props add SSR

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -18,7 +18,7 @@ const article = ({ article }) => {
   );
 };
 
-export const getStaticProps = async (context) => {
+export const getServerSideProps = async (context) => {
   const res = await fetch(`${server}/api/articles/${context.params.id}`);
 
   const article = await res.json();
@@ -30,18 +30,18 @@ export const getStaticProps = async (context) => {
   };
 };
 
-export const getStaticPaths = async () => {
-  const res = await fetch(`${server}/api/articles`);
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`${server}/api/articles`);
 
-  const articles = await res.json();
+//   const articles = await res.json();
 
-  const ids = articles.map((article) => article.id);
-  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+//   const ids = articles.map((article) => article.id);
+//   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
 
-  return {
-    paths,
-    fallback: false,
-  };
-};
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
 export default article;
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ articles }) {
   );
 }
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const res = await fetch(`${server}/api/articles`);
   const articles = await res.json();
 
